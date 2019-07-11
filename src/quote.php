5c7fbf5d2994121584dfedf80a7cfb858d5cfce5
Romain “Artefact2” Dal Maso <romain.dalmaso@artefact2.com>
2019-07-11 16:08:54 +0200

    Add Bourse Direct price history

--- src/quote.php
+++ src/quote.php
@@ -12,9 +12,8 @@
  * @param $date a strtotime()-parseable date. If 'now', a real time
  * quote will be used. If not, the closing price of the day will be
  * fetched.
- * @returns a price or null if no price could be fetched
  */
-function get_quote(array &$pf, string $ticker, string $date = 'now'): ?float {
+function get_quote(array &$pf, string $ticker, string $date = 'now'): float {
 	if(!isset($pf['lines'][$ticker])) {
 		fatal("Unknown ticker %s\n", $ticker);
 	}
@@ -24,22 +23,29 @@ function get_quote(array &$pf, string $ticker, string $date = 'now'): ?float {
 	$date = maybe_strtotime($date);
 
 	if(date('Y-m-d', $date) === ($today = date('Y-m-d'))) {
-		return get_boursorama_rt_quote($isin);
+		$q = get_boursorama_rt_quote($isin);
+		if($q !== null) return $q;
+		fatal("could not find intraday quote for %s\n", $ticker);
 	}
 
+	/* XXX: refactorable? */
 	$q = find_in_history($pf['hist'][$ticker] ?? [], $date);
 	if($q !== null) return $q;
 
-	$hist = get_boursorama_history($isin);
-	foreach($hist as $k => $v) {
-		$pf['hist'][$ticker][$k] = $v;
-	}
+	$brsh = get_boursorama_history($isin);
+	$bdh = get_bd_history($isin);
+
+	$pf['hist'][$ticker] = merge_histories(
+		$ticker,
+		$pf['hist'][$ticker],
+		merge_histories($ticker, $bdh, $brsh)
+	);
+
 	unset($pf['hist'][$ticker][$today]);
 	$q = find_in_history($pf['hist'][$ticker] ?? [], $date);
 	if($q !== null) return $q;
 
-	fprintf(STDERR, "pfm: could not find quote for %s at date %s\n", $ticker, date('Y-m-d', $date));
-	assert(0);
+	fatal("could not find quote for %s at date %s\n", $ticker, date('Y-m-d', $date));
 }
 
 function get_curl(string $url) {
@@ -115,6 +121,40 @@ function get_boursorama_history(string $isin): array {
 		});
 }
 
+function get_bd_id(string $isin): ?string {
+	return get_cached_thing('bd-id-'.$isin, -31557600, function() use($isin): ?string {
+		$c = get_curl('https://www.boursedirect.fr/api/search/'.$isin.'/lucky');
+		fwrite(STDOUT, '.');
+		curl_exec($c);
+		$loc = curl_getinfo($c, CURLINFO_EFFECTIVE_URL);
+		if(!preg_match('%/seance$%', $loc)) return null;
+		return substr($loc, 0, -strlen('/seance'));
+	});
+}
+
+function get_bd_history(string $isin): array {
+	return get_cached_thing('bd-hist-'.$isin, strtotime('tomorrow'), function() use($isin): array {
+			$loc = get_bd_id($isin);
+			if($loc === null) return [];
+			if(!preg_match('%-(?<ticker>[^-]+)-(?<currency>[^-]+)-(?<exchange>[^-]+)$%U', $loc, $m)) return [];
+
+			$c = get_curl('https://www.boursedirect.fr/api/instrument/download/history/'.$m['exchange'].'/'.$m['ticker'].'/'.$m['currency']);
+			fwrite(STDOUT, '.');
+			$csv = curl_exec($c);
+			$csv = explode("\n", trim($csv));
+			array_shift($csv);
+			$hist = [];
+			foreach($csv as $line) {
+				list($date,,,, $close) = explode(';', $line);
+				if($close === '') continue;
+
+				$hist[date('Y-m-d', strtotime(substr($date, 1, -1)))] = floatval($close);
+			}
+			fix_splits($isin, $hist);
+			return $hist;
+		});
+}
+
 function long_div(int $a, int $b) {
 	return [ (int)floor($a / $b), $a % $b ];
 }
@@ -173,4 +213,51 @@ function prev_open_day(int $ts): int {
 function find_in_history(array $hist, int $ts): ?float {
 	$ts = prev_open_day($ts);
 	return $hist[date('Y-m-d', prev_open_day($ts))] ?? null;
+}
+
+/* XXX: this is just heuristics */
+function fix_splits(string $isin, array &$h): void {
+	$prev = null;
+	foreach($h as $k => $v) {
+		if($prev === null) {
+			$prev = $v;
+			continue;
+		}
+
+		if($v / $prev > 3) {
+			$m = round($v / $prev);
+			assert(abs($v / $prev - $m) < .2);
+			notice("detected merge (%.0f:1) for %s at date %s\n", $m, $isin, $k);
+			$m = 1.0 / $m;
+		} else if($v / $prev < .33333) {
+			$m = round($prev / $v);
+			assert(abs($prev / $v - $m) < .2);
+			notice("detected split (1:%.0f) for %s at date %s\n", $m, $isin, $k);
+		} else {
+			$prev = $v;
+			continue;
+		}
+
+		foreach($h as $k2 => &$v2) {
+			if($k === $k2) break;
+			$v2 /= $m;
+		}
+
+		$prev = $v;
+	}
+}
+
+function merge_histories(string $ticker, array $h1, array $h2): array {
+	$h = $h1 + $h2;
+	ksort($h);
+	foreach($h as $k => $v) {
+		if(!isset($h1[$k]) || !isset($h2[$k])) continue;
+		$delta = abs(($h2[$k] - $h1[$k]) / $h1[$k]);
+		if($delta > 0.3) {
+			fatal("definite history mismatch for %s at %s: %.4f ≠ %.4f\n", $ticker, $k, $h1[$k], $h2[$k]);
+		} else if($delta > 0.001) {
+			notice("possible history mismatch for %s at %s: %.4f ≠ %.4f\n", $ticker, $k, $h1[$k], $h2[$k]);
+		}
+	}
+	return $h;
 }
